18e4671555e2b97be1e522917786f8768891f6af
Arnar <arnarrvk@gmail.com>
2019-12-03 10:08:23 +0000

    Create flight_attendant.py
    flight attendant structure added

---
+++ Arnar/flight_attendant.py
@@ -0,0 +1,13 @@
+class flight_attendant(employee):
+
+    def __init__ (self, supervisor, airplane_type):
+        self.supervisor = supervisor
+        self.airplane_type = airplane
+        
+    def set_supervisor():
+        pass
+
+    def set_airplane_type():
+        pass
+
+        
