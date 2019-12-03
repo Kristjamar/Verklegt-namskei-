fcae4cec7e9049fe3d16401d967bc26fe97ac67f
Kristjamar <42918705+Kristjamar@users.noreply.github.com>
2019-12-03 09:23:21 +0000

    fyrsta commit

---
+++ Kristján/pilot_class.py
@@ -0,0 +1,21 @@
+class Pilot(Employee):
+
+    def __init__(self, airplane, flight_license, airplane_type):
+        self.airplane = airplane
+        self.fligh_license = flight_license
+        self.airplane_type = airplane_type
+
+    def update_licence():
+        pass
+
+    def set_airplane():
+        pass
+
+    def update_aiplane_type():
+        pass
+
+def main():
+
+main()
+
+
