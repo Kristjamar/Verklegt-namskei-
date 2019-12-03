06564e8907a90d305e61a3976d12c777b7c31271
Arnar <arnarrvk@gmail.com>
2019-12-03 12:40:17 +0000

    created voyage

---
+++ Arnar/voyage.py
@@ -0,0 +1,21 @@
+class voyage(employee):
+    
+    def __init__(self, time_iceland, time_other):
+        
+    def double_distance():
+        pass
+    
+    def double_time_of_arriveal():
+        pass
+    
+    def check_pilot_licence():
+        pass
+    
+    def check_emp_day_voyage():
+        pass
+    
+    def store_voyage():
+        pass
+    
+    def update_fight_number():
+        pass
