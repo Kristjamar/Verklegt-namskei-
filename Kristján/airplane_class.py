8679bdc2ad192bb15fe006558ad0081eeea897b4
Kristjamar <42918705+Kristjamar@users.noreply.github.com>
2019-12-03 09:35:40 +0000

    beinagrind af airplane

---
+++ Kristján/airplane_class.py
@@ -0,0 +1,11 @@
+class Airplane():
+
+    def __init__(self, airplane_type, num_seats, manufacturer, name_of_airplane):
+        self.airplane_type = airplane_type
+        self.num_seats = num_seats
+        self.manufacturer = manufacturer
+        self.name_of_airplane = name_of_airplane
+
+    def create_airplane():
+        pass
+
