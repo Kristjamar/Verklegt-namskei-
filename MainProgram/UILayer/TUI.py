af523dd9a618263412b90f16f3e45f568d50edfd
Breki Hardarson <brekih@gmail.com>
2019-12-09 13:54:04 +0000

    Breyta mainmenu TUI o.fl

--- MainProgram/UILayer/TUI.py
+++ MainProgram/UILayer/TUI.py
@@ -6,12 +6,15 @@ QUIT = "q" or "Q"
 
 
 class Menu:
-    def __init__(self, title, option1, option2, option3, option4, how_many_options):
+    def __init__(self, title, option1, option2, option3, option4, option5, option6, option7, how_many_options):
         self.title = title
         self.option1 = option1
         self.option2 = option2
         self.option3 = option3
         self.option4 = option4
+        self.option5 = option5
+        self.option6 = option6
+        self.option7 = option7
         self.how_many_options = how_many_options
 
     def build_menu(self):
@@ -22,11 +25,23 @@ class Menu:
         print("1.{}".format(self.option1))
         print("2.{}".format(self.option2))
         print("3.{}".format(self.option3))
-        if self.how_many_options == 4:
+        if self.how_many_options >= 4:
             print("4.{}".format(self.option4))
+        if self.how_many_options >= 5:
+            print("5.{}".format(self.option5))
+        if self.how_many_options >= 6:
+            print("6.{}".format(self.option6))
+        if self.how_many_options == 7:
+            print("7.{}".format(self.option7))
         print()
-        print('[B]ack       [M]ain_menu')
+        if self.title == "Main Menu":
+            print("[Q]uit")
+        elif self.how_many_options <=4:
+            print('[B]ack       [M]ain_menu')
+        
+        
         return ""
+        '''Makes building a menu easier'''
     def mainmenu(self):
         print("#############################")
         print("------{}------".format(self.title))
@@ -38,14 +53,21 @@ class Menu:
         print()
         print("[Q]uit")
         return ""
+        
+    # def title(self):
+    #     print("#############################")
+    #     print("------{}------".format(self.title))
+    #     print("#############################")
+    #     print()
+    #     return ""
 
-main_menu = Menu("Main Menu", "Búa til", "Upplýsingar", "Uppfæra","", "3")
-register_menu = Menu("Nýskrá", "Skrá starfsmann", "Skrá áfangastað", "Skrá vinnuferð", "Skrá flugvél", 4)
-listing_menu = Menu("Upplýsingar", "Birta starfsmenn", "Birta áfangastaði", "Birta vinnuferðir", "Birta tegund flugvéla", 4)
-update_menu = Menu("Uppfæra", "Uppfæra starfsmenn", "Uppfæra áfangastaði", "Uppfæra vinnuferðir", "Uppfæra flugvél", 4)
-
+main_menu = Menu("Main Menu", "Búa til", "Upplýsingar", "Uppfæra","","","","", 3)
+register_menu = Menu("Nýskrá", "Skrá starfsmann", "Skrá áfangastað", "Skrá vinnuferð", "Skrá flugvél","","","", 4)
+listing_menu = Menu("Upplýsingar", "Birta starfsmenn", "Birta áfangastaði", "Birta vinnuferðir", "Birta tegund flugvéla","","","", 4)
+update_menu = Menu("Uppfæra", "Uppfæra starfsmenn", "Uppfæra áfangastaði", "Uppfæra vinnuferðir", "Uppfæra flugvél","","","", 4)
+register_emp_menu = Menu("Skrá starfsmann", "Fyrirnafn:(Jón)", "Eftirnafn:(Jónsson)", "Titill:(Flugstjóri)", "Heimanúmer:(4335858)","Farsími:(5812345)","Netfang:(Jonjonsson@nan.is)","Heimilisfang:(Bankastræti 5)", 7)
 def first_menu():
-    print(main_menu.mainmenu())
+    main_menu.build_menu()
     user_input = input("Veldu valmöguleika: ")
     if user_input == "1":
         Reg_menu()
@@ -105,6 +127,8 @@ def tmp_menu():
 
 
 def New_employee():
+    
+    register_emp_menu.build_menu()
     firstname = input("Fyrirnafn: ")
     lastname = input("Eftirnafn: ")
     ssn = input("Kennitala: ")
